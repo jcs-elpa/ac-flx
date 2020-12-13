6c19d1efac637f45359f7ba204a05b74c3710bac
JenChieh <jcs090218@gmail.com>
2020-12-13 21:42:36 +0800

    Update core.

---
+++ ac-flx.el
@@ -0,0 +1,42 @@
+;;; ac-flx.el --- flx support for auto-complete  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020  Shen, Jen-Chieh
+;; Created date 2020-12-13 21:41:13
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: flx support for auto-complete
+;; Keyword: ac auto-complete flx
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/jcs090218/ac-flx
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; flx support for auto-complete
+;;
+
+;;; Code:
+
+(defgroup ac-flx nil
+  "flx support for auto-complete."
+  :prefix "ac-flx-"
+  :group 'tools
+  :link '(url-link :tag "Repository" "https://github.com/jcs-elpa/ac-flx"))
+
+(provide 'ac-flx)
+;;; ac-flx.el ends here
